4d90f7bab889f331fc29cfc94dcc9b79634f3658
Henry Mascot <henrino3@gmail.com>
2026-05-14 09:03:45 +0100

    Publish Droid VibeProxy LiteLLM guide

--- src/content.config.ts
+++ src/content.config.ts
@@ -16,7 +16,7 @@ const blog = defineCollection({
 			image: z.string().optional(),
 			audio: z.string().optional(),
 			tags: z.array(z.string()).optional(),
-			author: z.enum(['ada']).optional(),
+			author: z.enum(['ada', 'book']).optional(),
 			draft: z.boolean().optional(),
 		}),
 });
